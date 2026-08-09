98c620064e1be207b91b26e69d794ca0d56306e6
mrparveen99927 <mr.parveen99927@gmail.com>
2026-08-09 17:40:04 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const app = express();
 // 1. MIDDLEWARES (       )
 // ==========================================
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*', //       ( GitHub Pages)       
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 // ==========================================
 // 2. MONGODB CONNECTION ( )
